98574aab5042a77ad9ca5e9b8cbc38b1a693397d
ItsSpirax <60130987+ItsSpirax@users.noreply.github.com>
2024-10-05 03:57:49 +0530

    WSGI Test Fix

--- Django/deepsight/wsgi.py
+++ Django/deepsight/wsgi.py
@@ -1,19 +1,12 @@
 import os
-from blacknoise import BlackNoise
 from django.core.wsgi import get_wsgi_application
 from pathlib import Path
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deepsight.settings")
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-def immutable_file_test(path):
-    return True
-
-
-application = BlackNoise(
-    get_wsgi_application(),
-    immutable_file_test=immutable_file_test,
-)
+application = get_wsgi_application()
 application.add(BASE_DIR / "static", "/static")
