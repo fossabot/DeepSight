5463dfa50142c61d4fd0c02435a55f2ab907ab7b
ItsSpirax <60130987+ItsSpirax@users.noreply.github.com>
2024-10-05 03:59:50 +0530

    Fix WSGI

--- Django/deepsight/wsgi.py
+++ Django/deepsight/wsgi.py
@@ -1,12 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from pathlib import Path
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deepsight.settings")
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 application = get_wsgi_application()
-application.add(BASE_DIR / "static", "/static")
